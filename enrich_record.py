8ccfaf87636dad6620c3fea360a670dccfa88c1d
Andrew Nelson <nelson.andrew.james@gmail.com>
2018-06-17 17:32:33 -0700

    use key

--- enrich_record.py
+++ enrich_record.py
@@ -8,7 +8,9 @@ import librosa
 import librosa.display
 import matplotlib.pyplot as plt
 import matplotlib.style as ms
+import key
 ms.use("seaborn-muted")
+
 
 def plots(
     y_h=None, 
@@ -20,7 +22,7 @@ def plots(
     ):
 
     fig = plt.figure(figsize=(12,8))
-    C = librosa.feature.chroma_stft(y=y_h, sr=sr, n_chroma=12)
+    C = librosa.feature.chroma_cqt(y=y_h, sr=sr, n_chroma=12)
     librosa.display.specshow(C, x_axis="time", y_axis="chroma", sr=sr)
     plt.colorbar()
     plt.title("chroma, " + short_fn + ", start_pos={0}".format(start_pos))
@@ -65,6 +67,8 @@ def get_features(
     # get simple features
     tempo, _ = librosa.beat.beat_track(y=y, sr=sr)
 
+    key = key.find_key(librosa.feature.chroma_cqt(y=y_h, sr=sr, n_chroma=12))
+
     # save
     data = {
         "y": y,
@@ -72,11 +76,11 @@ def get_features(
     }
     pickle.dump(data, open("data/" + "amp_sr_{0}.pkl".format(time_stamp), "wb"))
 
-    return tempo
+    return tempo, key
 
 def enrich_music(df):
 
-    tempo = df.apply(lambda x: 
+    tempo, key = df.apply(lambda x: 
         get_features(
             fn=x.file_name, 
             short_fn=x.short_file_name, 
@@ -86,7 +90,7 @@ def enrich_music(df):
         ), 
         axis=1)
 
-    return df.assign(tempo=tempo)
+    return df.assign(tempo=tempo, key=key[0], key_tone=key[1])
 
 if __name__ == "__main__":
     df = pd.read_csv("like_record.tsv", sep="\t")
